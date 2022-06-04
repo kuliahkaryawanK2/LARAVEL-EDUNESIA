cade99d62eb0eefc91dc1ecc47460bbf948f7edc
marsudii <marsudi124@gmail.com>
2022-06-04 11:05:32 +0700

    first commit

--- resources/views/Frontend/template/style.blade.php
+++ resources/views/Frontend/template/style.blade.php
@@ -4,18 +4,18 @@
     <title>Laravel Edunesia</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{url('frontend/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="/css1/fontawesome.css">
-    <link rel="stylesheet" href="/css1/templatemo-space-dynamic.css">
-    <link rel="stylesheet" href="/css1/animated.css">
-    <link rel="stylesheet" href="/css1/owl.css">
-    <link rel="stylesheet" href="/css/animate.css">
-    <link rel="stylesheet" href="/css/timeline.css">
-    <link rel="stylesheet" href="/css/service.css">
-    <link rel="stylesheet" href="/css/faq.css">
-    <link rel="stylesheet" href="/css/portofolio.css">
-    <link rel="stylesheet" href="/css/partner.css">
+    <link rel="stylesheet" href="{{url('frontend/css1/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css1/templatemo-space-dynamic.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css1/animated.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css1/owl.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/animate.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/timeline.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/service.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/faq.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/portofolio.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/partner.css')}}">
     
 <!--
